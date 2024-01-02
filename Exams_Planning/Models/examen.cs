41d41583621044e516b6d8cacc216bb09f53df9d
simobls <belassalsimo78@gmail.com>
2024-01-03 00:47:58 +0100

    'add'

---
+++ Exams_Planning/Models/examen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exams_Planning.Models
+{
+    public class examen
+    {
+        private int id;
+        private DateTime date;
+        private module module;
+        private lieu lieu;
+        private ICollection<enseignant> enseignants;
+
+        public int Id { get { return id; } set {  id = value; } }
+        public DateTime Date { get { return date; } set { date = value; } }
+        public module Module { get { return module; } set { module = value; } }
+        public lieu Lieu { get {  return lieu; } set {  lieu = value; } }
+        public ICollection<enseignant> Enseignants { get { return enseignants; } set { enseignants = value; } }
+    }
+}
---
+++ Exams_Planning/Models/lieu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exams_Planning.Models
+{
+    public class lieu
+    {
+        private int id;
+        private string name;
+        private int capacite;
+        private string type;
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public int Capacite { get {  return capacite; } set {  capacite = value; } }
+        public string Type { get { return type; } set { type = value; } }
+    }
+}
